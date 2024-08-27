61abb128c52b0f56580ea67bc58d1543e6686a6c
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-08-27 11:17:04 +0000

    [Finish]2-7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_24_142923) do
+ActiveRecord::Schema.define(version: 2024_08_26_140337) do
+
+  create_table "post_images", force: :cascade do |t|
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
